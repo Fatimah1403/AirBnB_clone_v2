304127ab39f589ddf63c91618e6d94fd5ebccf9c
Fatimah1403 <fattyhassan14@gmail.com>
2023-06-22 20:57:16 +0000

    Update on the classes

--- models/city.py
+++ models/city.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+import models
+from os import getenv
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    name = Column(string(128), nullable=False)
+    state_id = Column(string(60), nullable=False, ForeignKey(('state.id'))
+    places=relationship(
+    "Place",
+    backref="cities",
+     cascade="all, delete-orphan")
--- models/state.py
+++ models/state.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import models
+import os
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(string(128), nullable=False)
+    cities = relationship("City", cascade="all, delete, delete-orphan",
+                          backref="state")
+
+    @property
+    def cities(self):
+        """ that returns the list of City instances
+        with state_id equals to the current
+        State.id => It will be the FileStorage
+        relationship between State and City
+
+        """
+        Clist = []
+        for city in list(models.storage.all(City).values()):
+            if city.state_id == self.id:
+                city_list.append(city)
+        return Clist
