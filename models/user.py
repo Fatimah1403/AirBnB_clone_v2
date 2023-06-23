66496633d92bcc22ecadee391a96f712ed80a45c
Fatimah1403 <fattyhassan14@gmail.com>
2023-06-23 14:04:11 +0000

    User class updated

--- models/user.py
+++ models/user.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
+from models.city import City
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """This class defines a user by various attributes of user class"""
+
+    __tablename__ = "users"
+    email = Column(string(128), nullable=False)
+    password = Column(string(128), nullable=False)
+    first_name = Column(string(128))
+    last_name = Column(string(128))
+    places = relationship('Place', cascade='delete', backref='user')
+    reviews = relationship('Review', cascade='delete', backref='user')
