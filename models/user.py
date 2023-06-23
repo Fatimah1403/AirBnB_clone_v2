c2b8f7877f37110792260b074fc56b0996cdff52
Fatimah1403 <fattyhassan14@gmail.com>
2023-06-23 14:28:29 +0000

    User_models updated

--- models/user.py
+++ models/user.py
@@ -1,39 +1,34 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+""" holds class User"""
+import hashlib
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
-from models.city import City
 from os import getenv
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 
 class User(BaseModel, Base):
-    """ Represent a user for a MySQL database.
-
-    Attributes:
-        __tablename__: represents the table name, users
-        email: (sqlalchemy String): The user's email address.
-        password (sqlalchemy String): The user's password.
-        first_name (sqlalchemy String): The user's first name.
-        last_name (sqlalchemy String): The user's last name.
-        places (sqlalchemy relationship): The user-Place relationship.
-        reviews (sqlalchemy relationship): The user-Review relationship.
-
-    """
-
+    """Representation of a user """
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'users'
-        email = Column(string(128), nullable=False)
-        password = Column(string(128), nullable=False)
-        first_name = Column(string(128))
-        last_name = Column(string(128))
-        places = relationship('Place', cascade='delete', backref='user')
-        reviews = relationship('Review', cascade='delete', backref='user')
-
+        email = Column(String(128),
+                       nullable=False)
+        _password = Column('password',
+                           String(128),
+                           nullable=False)
+        first_name = Column(String(128),
+                            nullable=True)
+        last_name = Column(String(128),
+                           nullable=True)
+        places = relationship("Place",
+                              backref="user",
+                              cascade="all, delete-orphan")
+        reviews = relationship("Review",
+                               backref="user",
+                               cascade="all, delete-orphan")
     else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+        email = ""
+        _password = ""
+        first_name = ""
+        last_name = ""
