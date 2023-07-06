661abd09ffc6a1e90c5ed347274925aa8ad4c814
Fatimah1403 <fattyhassan14@gmail.com>
2023-07-06 19:53:29 +0000

    Task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-from fabric.api import local, run, put
-from datetime import datetime
-import os
+from fabric.api import local
 from time import strftime
+from datetime import date
 
 
 def do_pack():
-    """ A Fabric script that generates a .tgz archive
-    from the contents of the web_static"""
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    path = "versions/web_static_{}.tgz".format(date)
+    """ A script that generates archive the contents of web_static folder"""
 
+    filename = strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
-        local("tar -czvf {} web_static".format(path))
-        return path
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
+
+        return "versions/web_static_{}.tgz".format(filename)
+
     except Exception as e:
         return None
