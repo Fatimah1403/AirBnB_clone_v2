898b30d4963bf55f2b8a72d0d3b4c2bece8ab908
Fatimah1403 <fattyhassan14@gmail.com>
2023-07-07 19:03:26 +0000

    Nw changes

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,6 +17,6 @@ sudo echo "<html>
   </body>
 </html>" |sudo tee /data/web_static/releases/test/index.html
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
-sudo -R ubuntu:ubuntu /data/
-sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo chown  -R ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 sudo service nginx restart
