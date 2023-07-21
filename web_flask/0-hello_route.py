f21111ae7da148fc1d7c919a6a355a6ab73b41c6
Fatimah1403 <fattyhassan14@gmail.com>
2023-07-21 23:54:27 +0000

    Task 0, starting flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""  starts a Flask web application:
+
+Your web application must be listening on 0.0.0.0, port 5000
+
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ display “Hello HBNB!"""
+    return "<p>Hello HBNB!</>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
